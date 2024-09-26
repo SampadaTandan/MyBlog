44c9209fabee6a2745649724e5b9419f2079e96e
sampada <sampadatandan127@gmail.com>
2024-09-26 23:07:46 +0545

    Authentication II

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.post('/register',(req,res)=>{
-    res.json('test ok');
-    const {username, password}=req.body;
-    res.json({requestData:{username, password}})
+mongoose.connect(
+  "mongodb+srv://sampadatandan127:f5HCVnHeBBMALjhB@cluster0.ykwcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
-    res.status(200).json({ message: 'User registered successfully!', user: { username } });
-   
-})
-
-app.listen(4400, () => {
-    console.log('Server running on port 4400');
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.create({ username, password });
+  res.json(userDoc);
 });
+app.listen(4000);
+
+//mongodb+srv://sampadatandan127:<db_password>@cluster0.ykwcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//f5HCVnHeBBMALjhB
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,47 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
 
   async function register(ev) {
     ev.preventDefault();
-    if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
-    await fetch('http://localhost:4400/register', {
-      method: 'POST',
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
       body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
   }
+
+  // if (password !== confirmPassword) {
+  //   alert("Passwords do not match");
+  //   return;
+  // }
 
   return (
     <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input 
+      <input
         type="text"
         placeholder="Username"
         value={username}
-        onChange={ev => setUsername(ev.target.value)}
+        onChange={(ev) => setUsername(ev.target.value)}
       />
-      <input 
+      <input
         type="password"
         placeholder="Password"
         value={password}
-        onChange={ev => setPassword(ev.target.value)}
+        onChange={(ev) => setPassword(ev.target.value)}
       />
-      <input 
-        type="password"
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        onChange={ev => setConfirmPassword(ev.target.value)}
-      />
+      {/* <input
+      type="password"
+      placeholder="Confirm Password"
+      value={confirmPassword}
+      onChange={(ev) => setConfirmPassword(ev.target.value)}
+    /> */}
       <button type="submit">Register</button>
     </form>
   );
 };
-
 export default Register;
